7d453a30120962124563872be7197c4d528cc5bc
adarshadshetty09 <adarshadshetty09@gmail.com>
2025-05-09 10:56:00 +0530

    cloud Storage

---
+++ GCP_TERRAFORM/cloud_storage/main.tf
@@ -0,0 +1,28 @@
+resource "google_storage_bucket" "bucket" {
+  name                        = "bank-name"
+  project                     = var.project_id
+  location                    = var.location
+  storage_class               = var.storage_class
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+  labels                      = var.labels
+  force_destroy               = var.force_destroy
+  public_access_prevention    = var.public_access_prevention
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  dynamic "retention_policy" {
+    for_each = var.retention_policy != null ? [var.retention_policy] : []
+    content {
+      retention_period = retention_policy.value.retention_period
+    }
+  }
+
+  dynamic "encryption" {
+    for_each = var.kms_key_name != null && var.kms_key_name != "" ? [1] : []
+    content {
+      default_kms_key_name = var.kms_key_name
+    }
+  }
+}
---
+++ GCP_TERRAFORM/cloud_storage/variables.tf
@@ -0,0 +1,63 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Bucket location"
+  type        = string
+  default     = "US"
+}
+
+variable "storage_class" {
+  description = "Storage class of the bucket"
+  type        = string
+  default     = "STANDARD"
+}
+
+variable "uniform_bucket_level_access" {
+  description = "Whether to enforce uniform bucket-level access"
+  type        = bool
+  default     = true
+}
+
+variable "labels" {
+  description = "Labels to assign to the bucket"
+  type        = map(string)
+  default     = {
+    env = "staging"
+  }
+}
+
+variable "force_destroy" {
+  description = "Whether to allow destroying non-empty buckets"
+  type        = bool
+  default     = true
+}
+
+variable "public_access_prevention" {
+  description = "Public access prevention setting"
+  type        = string
+  default     = "enforced"
+}
+
+variable "versioning_enabled" {
+  description = "Whether versioning is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "retention_policy" {
+  description = "Retention policy for the bucket"
+  type = object({
+    retention_period = number
+  })
+  default = null
+}
+
+
+variable "kms_key_name" {
+  description = "The Cloud KMS key to use for bucket encryption"
+  type = string
+  default = null
+}
